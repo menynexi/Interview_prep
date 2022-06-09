7c538d478a3e75c80c233e11243abaed5cc24cee
menynexi <meny_nexi@outlook.com>
2022-06-09 09:46:32 -0600

    add ArrayStringProblems.java Solved isUnique problem with test cases

---
+++ src/Arrays_and_Strings/ArrayStringProblems.java
@@ -0,0 +1,27 @@
+package Arrays_and_Strings;
+
+class ArrayStringProblems{
+  public static boolean isUnique(String str){
+      if(str.length() == 0){
+        return true;
+      }
+      //this is going to allow us to treat uppercase and lowercase letters the same
+      str = str.toLowerCase();
+      for(int i = 0; i < str.length() - 1; i++){
+          for(int j = i+1; j < str.length();j++){
+              if(str.charAt(i) == str.charAt(j)){
+                  return false;
+              }
+          }
+      }
+      return true;
+  }
+
+  public static void main(String[] args) {
+     System.out.println(isUnique("Hello")); //return false
+     System.out.println(isUnique("car")); //return true
+     System.out.println(isUnique("carr")); //return false
+     System.out.println(isUnique("CcAr")); //return false
+     System.out.println(isUnique("")); //return true
+  }
+}
